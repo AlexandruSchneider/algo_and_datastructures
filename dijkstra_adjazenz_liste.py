c48338b8a77e393747c08f71c429bea5d70334c0
Alex Schneider <sjessandru.schneider@gmail.com>
2021-12-10 00:10:10 +0100

    dijkstra var name changed

--- dijkstra_adjazenz_liste.py
+++ dijkstra_adjazenz_liste.py
@@ -33,9 +33,9 @@ def Naechster_Knoten_Dijkstra(V: list, istFertig: list, abstand: list):
 def main():
     adjListe = [[0, (1, 4), (7, 8)], [1, (0, 4), (2, 8), (7, 11)], [2, (1, 8), (3, 7), (5, 4), (8, 2)], [3, (2, 7), (4, 9), (5, 14)], [4, (3, 9), (5, 10)], [5, (2, 4), (3, 14), (4, 10), (6, 2)], [6, (5, 2), (7, 1), (8, 6)], [7, (0, 8), (1, 11), (6, 1), (8, 7)], [8, (2, 2), (6, 6), (7, 7)]]
     start = 0
-    abstands, vorgaengers = dijkstraAdjazenzListe(adjListe, start)
-    print(f"{abstands=}")
-    print(f"{vorgaengers=}")
+    abstand, vorgaenger = dijkstraAdjazenzListe(adjListe, start)
+    print(f"{abstand=}")
+    print(f"{vorgaenger=}")
 
 
 if __name__ == "__main__":
